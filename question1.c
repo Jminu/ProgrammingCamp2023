752c9995375d1c5b4dded47a3eb1d1841322f4fb
Jin Minu <s9430939@naver.com>
2023-01-09 14:22:08 +0900

    question1_2

---
+++ question1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c;
+    int max;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if(a > b){
+        if(a > c)
+            max = a;
+        else
+            max = c;
+    }
+    else{ //a < b
+        if(b > c)
+            max = b;
+        else
+            max = c;
+    }
+
+    printf("가장 큰 수 : %d\n", max);
+
+    return 0;
+}
