63b6bf81c186211a4c51196464b3d2d823753750
Jin Minu <s9430939@naver.com>
2023-01-10 13:59:31 +0900

    아직해결못함

---
+++ question23.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct info{
+    char str[50];
+    struct info *link;
+} NODE;
+
+int main(void)
+{
+    NODE *p = NULL; //현재노드
+    NODE *prev = NULL; //이전노드
+    NODE *next = NULL; //다음노드
+    NODE *list = NULL;
+
+    char buffer[50];
+
+    while(1){
+        printf("문자열을 입력 : ");
+
+        if(){ //엔터 누르면 문자열 새로받음.
+            p = (NODE *)malloc(sizeof(NODE));
+            strcpy(p->str, buffer);
+
+            if(list == NULL){ //리스트 비어있으면
+                list = p;
+            }
+            else{ //이전 노드의 끝에 새로운 노드 붙임
+                prev->link = p;
+            }
+            p->link = NULL;
+            prev = p;
+        }
+        else{ //엔터말고 딴거 누르면 중지
+            break;
+        }
+    }
+
+    //출력
+    p = list;
+    while(p != NULL){
+        printf("%s", p->str);
+        p = p->link;
+    }
+
+    //동적 할당 반납
+    p = list;
+    while(p != NULL){
+        next = p->link;
+        free(p);
+        p = next;
+    }
+
+    return 0;
+}
