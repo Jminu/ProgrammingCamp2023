e9ec5872105e2e9feb07212e8e100b52fb8ab00c
Jin Minu <s9430939@naver.com>
2023-01-10 09:17:22 +0900

    19-2

--- question18-2.c
+++ question18-2.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+
+void remove_space(char *s);
 
 int main(void)
 {
+    char str[100];
+    gets(str); //scanf로 하면 단어한개만 받을 수 있음. gets()는 공백포함 문자열 받을 수 있음.
 
+    remove_space(str);
+
+    return 0;
+}
+
+void remove_space(char *s)
+{
+    char *token = strtok(s, " ");
+
+    while(token != NULL){
+        printf("%s", token);
+        token = strtok(NULL, " ");
+    }
 }
